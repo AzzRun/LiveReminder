987d428749306c0d9d39d8b5f02910009271f242
Quentin Martinez <quentinmartinez@MacBook-Pro-de-Quentin.local>
2018-08-25 12:34:19 +0200

    save

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
 import { NavController } from 'ionic-angular';
-
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+public tasks:any;
+  constructor(public navCtrl: NavController,public http: Http) {
+      this.http = http;
+      this.http.get("http://149.91.81.246/LiveTODOList/index.php?action=0").map(res => res.json()).subscribe(data =>{
+              this.tasks= data;//Bind data to items object
+            });
+            
   }
-
+  CheckboxClicked(item: any, $event) {
+    console.log('CheckboxClicked for ' + item.label);
+}
+datachanged(e:any){
+  console.log(e);
+  console.log(e.checked);  
+}
 }
